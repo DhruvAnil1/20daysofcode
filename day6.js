12b16b750a681899c54420fab621e7cdd79949f9
DhruvAnil1 <117620574+DhruvAnil1@users.noreply.github.com>
2023-01-17 21:28:18 +0530

    Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum.

---
+++ day6.js
@@ -0,0 +1,11 @@
+function sumTriple (x, y) {
+  if (x == y) {
+    return 3 * (x + y);
+    } 
+   else
+   {
+    return (x + y);
+   }
+ }
+console.log(sumTriple(100, 100));
+console.log(sumTriple(78, 46));
