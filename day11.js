569071bc5cdffc75017a0a5f17ace828c65452e3
DhruvAnil1 <117620574+DhruvAnil1@users.noreply.github.com>
2023-01-21 21:41:09 +0530

    Write a JavaScript program to check whether two numbers are in range 40..60 or in the range 70..100 inclusive.

---
+++ day11.js
@@ -0,0 +1,15 @@
+function numbers_ranges(x, y) {
+  if ((x >= 40 && x <= 60 && y >= 40 && y <= 60) 
+      || 
+      (x >= 70 && x <= 100 && y >= 70 && y <= 100))
+     {
+    return true;
+     } 
+    else 
+     {
+    return false;
+  }
+}
+
+console.log(numbers_ranges(41, 50));
+console.log(numbers_ranges(73, 92));
